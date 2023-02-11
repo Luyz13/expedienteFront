77534cebff325147fdd798a5efc4ce993e47eb32
reciclatorsfull@gmail.com <reciclatorsfull@gmail.com>
2023-02-11 09:11:30 -0600

    Se agrego los componentes header, footer, body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,13 +15,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask'
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './estructura-web/header/header.component';
+import { BodyComponent } from './estructura-web/body/body.component';
+import { FooterComponent } from './estructura-web/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PacientesComponent,
     ExpedienteComponent,
-    NoEncontradoComponent
+    NoEncontradoComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
